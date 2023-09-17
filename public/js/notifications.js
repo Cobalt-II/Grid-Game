bdf3a8964902a313db7944847ed7d7e7024ee0b7
Cobalt <97923189+Cobalt-II@users.noreply.github.com>
2023-09-17 12:19:17 -0400

    Update notifications.js

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -10,21 +10,15 @@ class notification {
 
 export let notifications = [];
 
-requestAnimationFrame(function clear() {
-    for (let count in notifications) {
-        let k = notifications[count].id;
-        setTimeout(() => {
-            for (let count in notifications) {
-                if (notifications[count].id === k) {
-                    notifications.splice(count, 1)
-                }
+export function pushNote(message, duration) {
+    notifications.push(new notification(message, duration, id));
+    let o = id;
+    id++;
+    setTimeout(() => {
+        for (let count in notifications) {
+            if (notifications[count].id === o) {
+                notifications.splice(count, 1)
             }
-        }, notifications[count].duration * 1000);
-    }
-    requestAnimationFrame(clear);
-});
-
-export function pushNote (message, duration) {
-notifications.push(new notification(message, duration, id));
-id++;
+        }
+    }, duration * 1000);
 };
